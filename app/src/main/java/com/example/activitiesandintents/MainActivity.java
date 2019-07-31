5a81f6429109804bffdc4a132e8838dfc2d481d7
mrdcharleston <mrdcharleston@gmail.com>
2019-07-31 13:01:03 -0700

    Added toolbar, up action button, and Intent from main activity to second activity.

--- app/src/main/java/com/example/activitiesandintents/MainActivity.java
+++ app/src/main/java/com/example/activitiesandintents/MainActivity.java
@@ -1,17 +1,48 @@
 package com.example.activitiesandintents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView messageReceived;
     private TextView message;
+    private EditText editText;
+    private Button sendMessage;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messageReceived = findViewById(R.id.message_received);
+        editText = findViewById(R.id.editText);
+        sendMessage = findViewById(R.id.sendMessage);
+
+        sendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG,"Button clicked");
+                Intent sendIntent = new Intent(MainActivity.this,Second_Activity.class);
+                sendIntent.setAction(Intent.ACTION_SEND);
+                String userText = editText.getText().toString();
+                sendIntent.putExtra(Intent.EXTRA_TEXT,userText);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/activitiesandintents/Second_Activity.java
+++ app/src/main/java/com/example/activitiesandintents/Second_Activity.java
@@ -1,7 +1,16 @@
 package com.example.activitiesandintents;
 
+
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.SpinnerAdapter;
+
 
 public class Second_Activity extends AppCompatActivity {
 
@@ -9,5 +18,17 @@ public class Second_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        if(getSupportActionBar() != null) {
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+
+
+
     }
 }
